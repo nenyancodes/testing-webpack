d3a96aeb0e60f4f210bd6f0b350dd46ce799b365
Ania Wójtowicz <wojtowicz.anna.maria+nenyan@gmail.com>
2020-06-17 15:28:01 +0200

    Create new.html site & add it to webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -63,5 +63,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/new.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "new.html",
+    }),
   ],
 };
